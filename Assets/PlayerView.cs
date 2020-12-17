a54b05b8a499e7e0184b9935ffc11f8fed4ee9df
AppsolusZero <6334424123@student.chula.ac.th>
2020-12-17 22:10:12 +0700

    Movement and view

---
+++ Assets/PlayerView.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerView : MonoBehaviour
+{
+    [SerializeField] private Transform flashLight;
+    void Update()
+    {
+        Vector3 LookDirection = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized;
+        LookDirection.z = 0f;
+        float angle = -CalculateAngle(LookDirection);
+        Debug.Log(angle);
+        Vector3 rot = new Vector3(0f, 0f, angle);
+        flashLight.rotation = Quaternion.Euler(rot); 
+    }
+
+    float CalculateAngle(Vector3 LookDir)
+    {
+        return Mathf.Atan2(LookDir.x, LookDir.y) * Mathf.Rad2Deg;
+    }
+}
