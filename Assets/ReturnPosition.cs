f2be87dafde50fbbd3b31ec9e4a803354cd028fe
AppsolusZero <6334424123@student.chula.ac.th>
2020-12-30 13:39:46 +0700

    Try using Grid to setup grid for navigation

---
+++ Assets/ReturnPosition.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class ReturnPosition : MonoBehaviour
+{
+    [SerializeField] private GridLayout grid;
+    [SerializeField] private Tilemap tilemap;
+
+    void Update()
+    {
+        Vector3 LookDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        Vector3Int thatGrid = grid.WorldToCell(LookDirection);
+        Vector3 currentPos = grid.CellToWorld(grid.WorldToCell(LookDirection));
+        currentPos.x += grid.cellSize.x/2f;
+        currentPos.y += grid.cellSize.y/2f;
+        Debug.Log(currentPos);
+        //tilemap.SetTile(thatGrid, )
+    }
+}
